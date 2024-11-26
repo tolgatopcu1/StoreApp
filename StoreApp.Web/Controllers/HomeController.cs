27fb52a1a3f30656222f6ce89ec2e88948f5769c
Tolga Topcu <tolgatpc200478@gmail.com>
2024-11-26 20:55:36 +0300

    MVC and Class Libary

---
+++ StoreApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
